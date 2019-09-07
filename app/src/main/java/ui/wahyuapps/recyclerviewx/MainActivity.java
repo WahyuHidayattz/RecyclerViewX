3f3520900ba31313d9d32d0cfa54ea6293d5250c
wahyuhidayattz <wahyuhidayatsmk2@gmail.com>
2019-09-07 21:36:39 +0700

    Initial commit

---
+++ app/src/main/java/ui/wahyuapps/recyclerviewx/MainActivity.java
@@ -0,0 +1,15 @@
+package ui.wahyuapps.recyclerviewx;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
