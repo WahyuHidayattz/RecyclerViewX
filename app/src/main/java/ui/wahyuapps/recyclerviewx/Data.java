f583d1343353471cfa507947bfc0c095cdc66759
wahyuhidayattz <wahyuhidayatsmk2@gmail.com>
2019-09-07 21:46:45 +0700

    migrate androidx

---
+++ app/src/main/java/ui/wahyuapps/recyclerviewx/Data.java
@@ -0,0 +1,8 @@
+package ui.wahyuapps.recyclerviewx;
+
+public class Data
+{
+	public String mTitle;
+	public String mSubtitle;
+	public int mImage;
+}
--- app/src/main/java/ui/wahyuapps/recyclerviewx/MainActivity.java
+++ app/src/main/java/ui/wahyuapps/recyclerviewx/MainActivity.java
@@ -3,13 +3,46 @@ package ui.wahyuapps.recyclerviewx;
 import android.app.Activity;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+	
+	RecyclerView mRecycler;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		mRecycler = findViewById(R.id.recycler_view);
+		setupRecyclerView();
     }
+	
+	private void setupRecyclerView(){
+		List<Data> data = new ArrayList<>();
+
+		String titles[];
+		String subtitles[];
+		int images[];
+
+		titles = getResources().getStringArray(R.array.item_title);
+		subtitles = getResources().getStringArray(R.array.item_subtitle);
+
+		for (int i = 0; i<titles.length; i++){
+			Data item = new Data();
+
+			item.mTitle = titles[i];
+			item.mSubtitle = subtitles[i];
+
+			data.add(item);
+		}
+
+		mRecycler.setHasFixedSize(true);
+		mRecycler.setLayoutManager(new
+								   LinearLayoutManager(this));
+		mRecycler.setAdapter(new Adapter(this, data));
+	}
     
 }
